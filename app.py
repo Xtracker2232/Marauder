21c3acbb6a54a3ab4bd033e8e4fd15b1fe441a7f
Xtracker2232 <carter06530@gmail.com>
2026-08-29 22:00:20 +0200

    Ajout des CGU avec lien dans le login

--- app.py
+++ app.py
@@ -100,3 +100,7 @@ with app.app_context():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
+    @app.route('/conditions')
+def conditions():
+    return render_template('conditions.html')
